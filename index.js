b2fd1732091fb2ff3bf9313daab6eb2342b4194e
ryanboris <ryan.boris@gmail.com>
2018-12-10 06:38:19 -0500

    Updates index.js and updates dependencies

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const server = require('./api/server.js');
+const winston = require('winston')
+const express = require('express')
+const app = express()
 
-const port = process.env.PORT || 5000;
+require('./startup/logging')()
+require('./startup/routes')(app)
+require('./startup/db')()
+require('./startup/config')()
+require('./startup/validation')()
 
-server.listen(port, () => console.log(`\n=== Server listening on port ${ port } ===`));
+const port = process.env.PORT || 5000
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`))
+
+module.exports = server
