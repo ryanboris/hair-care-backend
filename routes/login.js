a64249856c9d37c5d206d8a76cebefae5d6e5f5d
ryanboris <ryan.boris@gmail.com>
2018-12-10 06:35:43 -0500

    Builds routes skeletal structure and fills in server initializing code and provides todos for empty files

---
+++ routes/login.js
@@ -0,0 +1,6 @@
+//TODO: LOGIN USER DATA AND ROUTES ARE SEPERATED FROM USER DATA
+//TODO: LOGIN POST REQUESTS NEED TO BE WRITTEN
+//TODO: PULL IN MIDDLE WARE  AND PROTECT ACCESSED RESOURCES
+//TODO: WRITE HTTP STATUS MESSAGES FOR !username || !password and send appropriate codes to client
+//TODO: catch errors 
+//Note to self: utilize async/await && try/catch
